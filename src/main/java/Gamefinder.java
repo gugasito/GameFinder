88162c65c086712274745a3420dbe31a95a4ab90
Gustavo Soto <gsoto998@gmail.com>
2023-05-24 22:12:33 -0400

    Método agregarJuego() con método de escritura mejorada en el archivo GameFinder.txt

--- src/main/java/Gamefinder.java
+++ src/main/java/Gamefinder.java
@@ -1,41 +1,75 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.List;
 
 public class Gamefinder {
     public static void main(String[] args) {
-        verJuegos();
+        leer("GameFinder.txt");
+        agregarJuego();
+        leer("Gamefinder.txt");
     }
 
-    private static void verJuegos() {
-        FileReader r = null;
+    private static void agregarJuego() {
+        Juego newGame = new Juego("Valorant", 2021, "fps", "RiotGames", true);
         try {
-            r = new FileReader("GameFinder.txt");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            FileWriter archivo = new FileWriter("GameFinder.txt", true);
+            try (BufferedWriter bw = new BufferedWriter(archivo)) {
+                bw.write("\n" + newGame.getNombre() + " " + newGame.getAño() + " " + newGame.getGenero() + " " + newGame.getDistribuidora() + " " + newGame.isFreeTP());
+            }
+            archivo.close();
+        } catch (Exception ignored) {
         }
-        BufferedReader buffer = new BufferedReader(r);
-        String temp = "";
-        while (temp != "nulL") {
-            try {
+
+    }
+
+
+    public static void leer(String nombrearchivo) {
+        try {
+
+            FileReader r = new FileReader(nombrearchivo);
+            BufferedReader buffer = new BufferedReader(r);
+
+            String temp = "";
+
+            while (temp != null) {
+
                 temp = buffer.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                String x;
+                x = temp;
+                String[] arreglo = x.split(" ");
+                for (int i = 0; i < arreglo.length; i++) {
+                    System.out.println("" + arreglo[i]);
+                }
+                if (temp == null)
+                    break;
             }
-            String x;
-            x = temp;
-            String[] arreglo = x.split(" ");
-
-            for (int i = 0; i < arreglo.length; i++) {
-
-                System.out.println("" + arreglo[i]);
-            }
-
+        } catch (Exception e) {
+            System.out.println();
         }
     }
+
+    public static boolean grabar(List<Juego> Datos) {
+        try {
+            FileWriter archivo = new FileWriter("GameFinder.txt", true);
+            try (BufferedWriter bw = new BufferedWriter(archivo)) {
+                for (Juego dato : Datos) {
+                    bw.write(dato + "\n");
+                }
+                bw.close();
+            }
+            archivo.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
+
+
+
+
+
 
 
 
