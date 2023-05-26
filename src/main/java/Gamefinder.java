75a1335203dcf59fc0f9e199af302495eab4b3fb
BenjaminCruzado <b.cruzado01@ufromail.cl>
2023-05-26 16:39:40 -0400

    Subo clase Usuario y las funciones de buscar juego por año y buscar administradores

--- src/main/java/Gamefinder.java
+++ src/main/java/Gamefinder.java
@@ -5,7 +5,13 @@ public class Gamefinder {
     public static void main(String[] args) {
         agregarJuego("Valorant", 2021, "Shooter", "RiotGames", true);
         agregarJuego("League of Leguends", 2013, "Moba", "Riot Games", true);
+        agregarUsuario("Benja", Usuario.Rol.Administrador);
+        agregarUsuario("Guga", Usuario.Rol.Administrador);
+        agregarUsuario("Diego", Usuario.Rol.Usuario);
+        agregarUsuario("Samuel", Usuario.Rol.Administrador);
         leer("Gamefinder.txt");
+        buscarPorAño("2021");
+        leerAdiministrador("Administrador");
     }
 
     private static void agregarJuego(String nombre, int año, String genero, String distribuidora, boolean freeTP) {
@@ -14,6 +20,18 @@ public class Gamefinder {
             FileWriter archivo = new FileWriter("GameFinder.txt", true);
             try (BufferedWriter bw = new BufferedWriter(archivo)) {
                 bw.write("\n" + newGame.getNombre() + " " + newGame.getAño() + " " + newGame.getGenero() + " " + newGame.getDistribuidora() + " " + newGame.isFreeTP());
+            }
+            archivo.close();
+        } catch (Exception ignored) {
+        }
+
+    }
+    private static void agregarUsuario(String nombre, Usuario.Rol rol) {
+        Usuario newUsuario = new Usuario(nombre, rol);
+        try {
+            FileWriter archivo = new FileWriter("GameFinderUsuarios.txt", true);
+            try (BufferedWriter bw = new BufferedWriter(archivo)) {
+                bw.write("\n" + newUsuario.getNombre() + " " + newUsuario.isRol());
             }
             archivo.close();
         } catch (Exception ignored) {
@@ -62,6 +80,35 @@ public class Gamefinder {
         }
         return true;
     }
+    public static void buscarPorAño(String año){
+        try{
+            try (BufferedReader br = new BufferedReader(new FileReader("Gamefinder.txt"))){
+                String linea;
+                while ((linea = br.readLine()) != null){
+                    if(linea.contains(año)){
+                        System.out.println(linea);
+                    }
+                }
+            }
+        }catch (Exception ignored){
+            System.out.println("error");
+        }
+    }
+    public static void leerAdiministrador(String rol){
+        try{
+            try (BufferedReader br = new BufferedReader(new FileReader("GamefinderUsuarios.txt"))){
+                String linea;
+                while ((linea = br.readLine()) != null){
+                    if(linea.contains(rol)){
+                        System.out.println(linea);
+                    }
+                }
+            }
+        }catch (Exception ignored){
+            System.out.println("error");
+        }
+    }
+
 
     public static void borrarJuego() {
 
